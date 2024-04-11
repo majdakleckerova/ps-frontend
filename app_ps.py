cbcc409e9643173d195e2299a37eae9339c88b36
majdakleckerova <161514333+majdakleckerova@users.noreply.github.com>
2024-04-11 13:33:38 +0200

    Update 2  app_ps.py

--- app_ps.py
+++ app_ps.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_title="Hledání chyb",page_icon=":left_speech_bubble:",layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(page_title="Hledání chybiček",page_icon=":left_speech_bubble:",layout="wide", initial_sidebar_state="expanded")
 
 hide_streamlit_style = """<style>
                 {visibility: hidden;}
